8fc596a4a7838761eb1b0e6e3d1542554ec82127
ugur2889 <beyza569>
2019-03-20 14:23:37 +0300

    add base entity adn common interface

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -1,15 +1,12 @@
 package guru.springframework.sfgpetclinic.services;
 
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Owner,Long> {
 
-    Vet findById(Long id);
 
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
